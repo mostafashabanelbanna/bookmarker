49917957241d3856fc8b2d06c9f9eba35f190278
Mostafa-shaban <dashassas@gmail.com>
2018-10-16 17:33:59 +0200

    create fetchBookmark function

--- js/main.js
+++ js/main.js
@@ -27,5 +27,29 @@ function saveBookmark() {
 		bookmarks.push(bookmark);
 		// Reset back to localStorage
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
-	}
+	};
+
 };
+
+// Fetch bookmarks
+function fetchBookmarks() {
+		// Get bookmarks from localStorage
+		var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));
+		// Get output id 
+		var bookmarksResults = document.getElementById('BookmarksResults');
+
+		// Build output
+		bookmarksResults.innerHTML = '';
+		for(var i = 0; i < bookmarks.length; i++){
+			var name = bookmarks[i].name;
+			var url = bookmarks[i].url;
+			bookmarksResults.innerHTML += '<div class="well">'+
+		                                  '<h3>'+name+
+		                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
+		                                  ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+		                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
+		                                  '</h3>'+
+		                                  '</div>';
+
+		}
+	}
