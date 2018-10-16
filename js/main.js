a8a6acff473ff855a636a5684eabb7d171d9e193
Mostafa-shaban <dashassas@gmail.com>
2018-10-16 20:03:22 +0200

    create deleteBookmark function

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@
 document.getElementById('myForm').addEventListener('submit', saveBookmark);
 
 // Save bookmark
-function saveBookmark() {
+function saveBookmark(e) {
 	// Get form values
 	var siteName = document.getElementById('siteName').value;
 	var siteUrl = document.getElementById('siteUrl').value;
@@ -29,8 +29,27 @@ function saveBookmark() {
 		localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
 	};
 
+	// Re-fetch bookmarks
+	fetchBookmarks();
+
+	e.preventDefault();
 };
 
+// Delete bookmark
+function deleteBookmark(url) {
+	// Get bookmarks from localStorage
+	var bookmarks = JSON.parse(localStorage.getItem('bookmarks'));		
+	// Loop throught bookmarks
+	for (var i = 0; i < bookmarks.length; i++) {
+		if(bookmarks[i].url == url){
+			// remove from arry
+			bookmarks.splice(i, 1);
+		}
+	}
+	localStorage.setItem('bookmarks', JSON.stringify(bookmarks));
+	// Re-fetch bookmarks
+	fetchBookmarks();
+}
 // Fetch bookmarks
 function fetchBookmarks() {
 		// Get bookmarks from localStorage
@@ -46,7 +65,7 @@ function fetchBookmarks() {
 			bookmarksResults.innerHTML += '<div class="well">'+
 		                                  '<h3>'+name+
 		                                  ' <a class="btn btn-default" target="_blank" href="'+url+'">Visit</a> ' +
-		                                  ' <a class="btn btn-default" target="_blank" href="'+addhttp(url)+'">Visit</a> ' +
+		                                  ' <a class="btn btn-default" target="_blank" ">Visit</a> ' +
 		                                  ' <a onclick="deleteBookmark(\''+url+'\')" class="btn btn-danger" href="#">Delete</a> ' +
 		                                  '</h3>'+
 		                                  '</div>';
